1d92c5762e337827c10dbdf826d92af312c8e78c
singletary.aliyah@gmail.com <singletary.aliyah@gmail.com>
2018-08-30 05:15:43 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,5 +6,5 @@ function updatePlaylist (playlist, artistName,songTitle){
 }
 function removeFromPlaylist (playlist,artistName){
  playlist[artistName] = playlist
-  delete artistName.playlist;
+  delete playlist.artistName;
 }
